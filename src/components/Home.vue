09281f54b7ac7c40036c5ce09acdebed36deecf8
lifetreeandwater <shenhongbinshb@163.com>
2020-11-21 20:36:55 +0800

    update:home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -40,12 +40,12 @@ export default {
     margin-bottom: 40px;
   }
   
-  .el-container:nth-child(5) .el-aside,
+  /* .el-container:nth-child(5) .el-aside,
   .el-container:nth-child(6) .el-aside {
     line-height: 260px;
   }
   
   .el-container:nth-child(7) .el-aside {
     line-height: 320px;
-  }
+  } */
 </style>
